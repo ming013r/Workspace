b22631b44d4f7ec4e074c2d86b60ac07cea878dc
MingAir <ming013r@gmail.com>
2018-02-07 20:00:09 +0800

    Login Feature

---
+++ Android/HWriteQuiz/app/src/main/java/com/example/yongyouming/hwritequiz/ConfigFile.java
@@ -0,0 +1,16 @@
+package com.example.yongyouming.hwritequiz;
+
+/**
+ * Created by ming on 2017/11/25.
+ */
+
+public class ConfigFile {
+    public static boolean enableBLE=true;
+    public static int BLEinterval=300;
+
+    public static String version="1";
+
+
+    public static String IP="140.130.33.228";
+}
+
--- Android/HWriteQuiz/app/src/main/java/com/example/yongyouming/hwritequiz/CourseEntry.java
+++ Android/HWriteQuiz/app/src/main/java/com/example/yongyouming/hwritequiz/CourseEntry.java
@@ -3,11 +3,11 @@ package com.example.yongyouming.hwritequiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class CourseEntry extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_course_entry);
     }
 }
