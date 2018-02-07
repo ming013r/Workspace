9fdd9ffefe8cfbff1af7d3f39a1a4ee9b9dba8b4
MingAir <ming013r@gmail.com>
2018-02-07 22:58:26 +0800

    CourseIndex

--- Android/HWriteQuiz/app/src/main/java/com/example/yongyouming/hwritequiz/CourseIndex.java
+++ Android/HWriteQuiz/app/src/main/java/com/example/yongyouming/hwritequiz/CourseIndex.java
@@ -1,13 +1,33 @@
 package com.example.yongyouming.hwritequiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CourseIndex extends AppCompatActivity {
 
+    Intent it_prev;
+    String token;
+    int cid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_index);
+
+        setParameters();
+
+    }
+
+
+
+
+
+    void setParameters()
+    {
+        it_prev=getIntent();
+
+        token=it_prev.getStringExtra("token");
+        cid=it_prev.getIntExtra("cid",-1);
     }
 }
